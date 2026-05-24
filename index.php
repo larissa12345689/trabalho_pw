40113363026cff91da4c1490355d84849f4db536
larissa12345689 <larissa.silva652@aluno.ce.gov.br>
2026-05-24 00:00:58 -0300

    feito um pouquinho do design

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PetCare - Cuidado Completo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- <nav class="navbar">
+        <ul class="nav-pages">
+            <li>Início</li>
+            <li>Agendamentos</li>
+            <li>Cadastrar</li>
+        </ul>
+    </nav> -->
+
+    <nav class="navbar">
+
+    <div class="nav-logo">
+    </div>
+
+    <ul class="nav-pages">
+        <li><bu>Início</bu></li>
+        <li>Agendamentos</li>
+        <li>Cadastrar</li>
+        <li>Login</li>
+    </ul>
+</nav>
+</body>
+</html>
+
+</body>
+</html>
