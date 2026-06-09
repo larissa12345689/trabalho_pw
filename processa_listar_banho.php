e5dcabd20a727c2ebd7c322cfa8ebd328a123978
larissa12345689 <larissa.silva652@aluno.ce.gov.br>
2026-06-09 16:45:22 -0300

    tirando arquivos desnecessarios ou duplicados

--- processa_listar_banho.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../conexao.php";
-
-$stmt = $conn->prepare("SELECT * FROM banho");
-$stmt->execute();
-
-$agendamentos = $stmt->fetchAll(PDO::FETCH_ASSOC);                         
-
-?>
